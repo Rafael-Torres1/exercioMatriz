49af612e04c66a29a2b39407c8a167187c8fddb0
Rafael Torres <rafaeltorresomega6@gmail.com>
2026-08-22 13:57:06 -0300

    feat: trabalho multiplicação de matrizes comentado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Declração de matriz A (2x4)
         int[][] A = {
                 {2, 6, 1, 8},
                 {3, 5, 4, 1}
         };
 
+        // Declaração de matriz N (4x2)
         int[][] B = {
                 {3, 6},
                 {1, 7},
@@ -13,16 +14,20 @@ public class Main {
                 {1, 3}
         };
 
+        // Declração da matriz que armazena linha e coluna de A e B
         int[][] C = new int[2][2];
 
+        // Laço de repetição responsável pela multiplicação da linha A pela coluna B
         for (int i = 0; i < 2; i++ ){
             for (int j = 0; j < 2; j++){
                 for (int k = 0; k < 4; k++){
+                    // Multiplicação armazenada na variável C
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
 
+        // Laço de repetição responsável por mostrar no terminal o resultado da multiplicação
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 System.out.printf("%4d ", C[i][j]);
